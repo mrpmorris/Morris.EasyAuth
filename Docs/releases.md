085c39937a7de507342efafe809958278494c1f0
Peter Morris <mrpmorris@gmail.com>
2022-05-26 17:24:43 +0100

    Update version to 0.2.0

--- Docs/releases.md
+++ Docs/releases.md
@@ -1,3 +1,11 @@
 # Releases
 
+## New in 0.2
+ * Auto register so that injecting `HttpClient` or asking for an instance via
+    `IHttpClientFactory` with no name returns a client that will redirect to the
+    sign-in page if not authenticated.
+ * Exposed `AuthorizingMessageHandler` so consuming apps can use it in
+    `Services.AddHttpClient(name: "SomeName", ...).AddHttpMessageHandler<T>();`
+
 ## New in 0.1
+ * The ability for WASM to call to a server and ask for credentials, passing a cookie
--- Source/Lib/Morris.BasicAuth.Blazor.Web.Client/AuthorizatingMessageHandler.cs
+++ Source/Lib/Morris.BasicAuth.Blazor.Web.Client/AuthorizatingMessageHandler.cs
@@ -34,32 +34,3 @@ public class AuthorizingMessageHandler : DelegatingHandler
 		return response;
 	}
 }
-
-//internal class InternalAuthorizingMessageHandler : HttpClientHandler
-//{
-//	private readonly NavigationManager NavigationManager;
-//	private readonly ClientAuthOptions AuthOptions;
-
-//	public InternalAuthorizingMessageHandler(
-//		NavigationManager navigationManager,
-//		ClientAuthOptions authOptions)
-//	{
-//		NavigationManager = navigationManager ?? throw new ArgumentNullException(nameof(navigationManager));
-//		AuthOptions = authOptions ?? throw new ArgumentNullException(nameof(authOptions));
-//	}
-
-//	protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-//	{
-//		HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
-//		// Blazor WASM returns a zero StatusCode from JS if it performs a redirect
-//		if (response.StatusCode != System.Net.HttpStatusCode.Unauthorized && response.StatusCode != 0)
-//			return response;
-
-//		string returnUrl = HttpUtility.UrlEncode(NavigationManager.Uri);
-//		string url = string.Format(AuthOptions.SignInUrlTemplate, returnUrl);
-//		NavigationManager.NavigateTo(url, forceLoad: true);
-//		await Task.Delay(30_000); // This just prevents us from returning a result to the requester
-//		return response;
-//	}
-//}
-
